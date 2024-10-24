f4d428b7b67f494d0cfff4521d9490e81a8ad6b2
abdulrehmanafzal-webdeveloper <abdulrehmanafzal151@gmail.com>
2024-10-24 13:15:17 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+     './src/component/Navbar.js',
+     './src/component/Weather.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
